c73fdd9e5615812ac1c868b36aee5ee134b438c5
Maximus Fernandez <maximusfernandez98@gmail.com>
2026-01-29 19:42:25 -0500

    Update to Next.js

---
+++ maximusf-portfolio/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Header /> // renders the Header component
+  )
+}
+
+export default App;// 
---
+++ maximusf-portfolio/src/components/Header.tsx
@@ -0,0 +1,15 @@
+// components/Header.tsx
+
+// creates a header component for the portfolio website
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <h1>My Portfolio</h1>
+            </nav>
+        </header>
+    );
+};
+
+// makes header component available for import in other files
+export default Header;
